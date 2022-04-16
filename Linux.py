1f40164283659e42c44f0ce3553d33e7f6fad932
DevDodger <88220593+DevDodger@users.noreply.github.com>
2022-04-15 17:41:39 -0700

    Create Linux.py

---
+++ Linux.py
@@ -0,0 +1,49 @@
+def get_linux_saved_wifi_passwords(verbose=1):   
+    """Extracts saved Wi-Fi passwords saved in a Linux machine, this function extracts data in the
+    `/etc/NetworkManager/system-connections/` directory
+    Args:
+        verbose (int, optional): whether to print saved profiles real-time. Defaults to 1.
+    Returns:
+        [list]: list of extracted profiles, a profile has the fields ["ssid", "auth-alg", "key-mgmt", "psk"]
+    """
+    network_connections_path = "/etc/NetworkManager/system-connections/"
+    fields = ["ssid", "auth-alg", "key-mgmt", "psk"]
+    Profile = namedtuple("Profile", [f.replace("-", "_") for f in fields])
+    profiles = []
+    for file in os.listdir(network_connections_path):
+        data = { k.replace("-", "_"): None for k in fields }
+        config = configparser.ConfigParser()
+        config.read(os.path.join(network_connections_path, file))
+        for _, section in config.items():
+            for k, v in section.items():
+                if k in fields:
+                    data[k.replace("-", "_")] = v
+        profile = Profile(**data)
+        if verbose >= 1:
+            print_linux_profile(profile)
+        profiles.append(profile)
+    return profiles
+
+
+def print_linux_profile(profile):
+    """Prints a single profile on Linux"""
+    print(f"{str(profile.ssid):25}{str(profile.auth_alg):5}{str(profile.key_mgmt):10}{str(profile.psk):50}")
+    def print_linux_profiles(verbose):
+    """Prints all extracted SSIDs along with Key (PSK) on Linux"""
+    print("SSID                     AUTH KEY-MGMT  PSK")
+    print("-"*50)
+    get_linux_saved_wifi_passwords(verbose)
+    def print_profiles(verbose=1):
+    if os.name == "nt":
+        print_windows_profiles(verbose)
+    elif os.name == "posix":
+        print_linux_profiles(verbose)
+    else:
+        raise NotImplemented("Code only works for either Linux or Windows")
+    
+    
+if __name__ == "__main__":
+    print_profiles()
+
+# run
+#   $ python get_wifi_passwords.py
